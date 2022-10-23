b31490251f69b10525b19470e80a1cd8f36405fa
Mikhail Letov <mletov@yandex.ru>
2022-10-23 22:25:02 +0300

    ДЗ: XML и JSON

--- homeworks/homework-9/hw_xml_json_tasks-188-97b45f.sql
+++ homeworks/homework-9/hw_xml_json_tasks-188-97b45f.sql
@@ -41,13 +41,154 @@ USE WideWorldImporters
 Сделать два варианта: с помощью OPENXML и через XQuery.
 */
 
-напишите здесь свое решение
+-- ***************************************************************************************************************************************************
+-- OPEN XML
+-- ***************************************************************************************************************************************************
+
+IF OBJECT_ID('tempdb..#tmp_StockItems') IS NOT NULL BEGIN DROP TABLE #tmp_StockItems; END
+
+-- Переменная, в которую считаем XML-файл
+DECLARE @xmlDocument  xml
+
+-- Считываем XML-файл в переменную
+SELECT @xmlDocument = BulkColumn
+FROM OPENROWSET
+(BULK 'F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItems-188-1fb5df.xml', 
+ SINGLE_CLOB)
+as data 
+
+-- Проверяем, что в @xmlDocument
+SELECT @xmlDocument as [@xmlDocument]
+
+DECLARE @docHandle int
+EXEC sp_xml_preparedocument @docHandle OUTPUT, @xmlDocument
+
+SELECT *
+INTO #tmp_StockItems
+FROM OPENXML(@docHandle, N'/StockItems/Item')
+WITH 
+( 
+	[StockItemName] NVARCHAR(100)  '@Name',
+	[SupplierID] INT 'SupplierID',
+	[UnitPackageID] INT 'Package/UnitPackageID',
+	[OuterPackageID] INT 'Package/OuterPackageID',
+	[QuantityPerOuter] INT 'Package/QuantityPerOuter',
+	[TypicalWeightPerUnit] DECIMAL(18,2) 'Package/TypicalWeightPerUnit',
+	[LeadTimeDays] INT 'LeadTimeDays',	
+	[IsChillerStock] BIT 'IsChillerStock',
+	[TaxRate] DECIMAL(18,3) 'TaxRate',
+	[UnitPrice] DECIMAL(18,2) 'UnitPrice'
+)
+
+SELECT *
+FROM #tmp_StockItems
+
+
+MERGE Warehouse.StockItems AS Base
+USING #tmp_StockItems AS Source
+ON (Base.StockItemName = Source.StockItemName) --Условие объединения
+WHEN MATCHED THEN --Если истина (UPDATE)
+UPDATE SET StockItemName = Source.StockItemName, 
+		SupplierID = Source.SupplierID, 
+		UnitPackageID = Source.UnitPackageID, 
+		OuterPackageID = Source.OuterPackageID, 
+		QuantityPerOuter = Source.QuantityPerOuter, 
+		TypicalWeightPerUnit = Source.TypicalWeightPerUnit, 
+		LeadTimeDays = Source.LeadTimeDays,	
+		IsChillerStock = Source.IsChillerStock, 
+		TaxRate = Source.TaxRate, 
+		UnitPrice = Source.UnitPrice
+WHEN NOT MATCHED THEN
+
+INSERT
+([StockItemName], [SupplierID], [UnitPackageID], [OuterPackageID], [QuantityPerOuter], [TypicalWeightPerUnit], [LeadTimeDays],	[IsChillerStock], [TaxRate], [UnitPrice], [LastEditedBy])
+VALUES
+(Source.StockItemName, Source.SupplierID, Source.UnitPackageID, Source.OuterPackageID, Source.QuantityPerOuter, Source.TypicalWeightPerUnit, Source.LeadTimeDays,	Source.IsChillerStock, Source.TaxRate, Source.UnitPrice, 1)
+OUTPUT $action AS [OperationName]
+				  , Inserted.StockItemName
+                  , Deleted.StockItemName; 
+				  
+-- ***************************************************************************************************************************************************				  
+-- XQuery
+-- ***************************************************************************************************************************************************
+
+IF OBJECT_ID('tempdb..#tmp_StockItems') IS NOT NULL BEGIN DROP TABLE #tmp_StockItems; END
+
+-- Переменная, в которую считаем XML-файл
+DECLARE @xmlDocument  xml
+
+-- Считываем XML-файл в переменную
+SELECT @xmlDocument = BulkColumn
+FROM OPENROWSET
+(BULK 'F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItems-188-1fb5df.xml', 
+ SINGLE_CLOB)
+as data 
+
+-- Проверяем, что в @xmlDocument
+SELECT @xmlDocument as [@xmlDocument]
+
+DECLARE @docHandle int
+EXEC sp_xml_preparedocument @docHandle OUTPUT, @xmlDocument
+
+SELECT *
+INTO #tmp_StockItems
+FROM 
+(
+
+	SELECT
+				   StockItems.Item.value(N'@Name', N'NVARCHAR(100)') AS StockItemName
+				,  StockItems.Item.value(N'SupplierID[1]', N'INT') AS SupplierID
+				,  StockItems.Item.value(N'(Package[1]/UnitPackageID[1])', N'INT') AS UnitPackageID
+				,  StockItems.Item.value(N'(Package[1]/OuterPackageID[1])', N'INT') AS OuterPackageID
+				,  StockItems.Item.value(N'(Package[1]/QuantityPerOuter[1])', N'INT') AS QuantityPerOuter
+				,  StockItems.Item.value(N'(Package[1]/TypicalWeightPerUnit)[1]', N'DECIMAL(18,2)') AS TypicalWeightPerUnit
+				,  StockItems.Item.value(N'LeadTimeDays[1]', N'INT') AS LeadTimeDays
+				,  StockItems.Item.value(N'IsChillerStock[1]', N'BIT') AS IsChillerStock
+				,  StockItems.Item.value(N'TaxRate[1]', N'DECIMAL(18,3)') AS TaxRate
+				,  StockItems.Item.value(N'UnitPrice[1]', N'DECIMAL(18,2)') AS UnitPrice
+	FROM       @xmlDocument.nodes(N'/StockItems/Item') AS StockItems(Item)
+
+) AS t1
+
+SELECT *
+FROM #tmp_StockItems
+
+
+MERGE Warehouse.StockItems AS Base
+USING #tmp_StockItems AS Source
+ON (Base.StockItemName = Source.StockItemName) --Условие объединения
+WHEN MATCHED THEN --Если истина (UPDATE)
+UPDATE SET StockItemName = Source.StockItemName, 
+		SupplierID = Source.SupplierID, 
+		UnitPackageID = Source.UnitPackageID, 
+		OuterPackageID = Source.OuterPackageID, 
+		QuantityPerOuter = Source.QuantityPerOuter, 
+		TypicalWeightPerUnit = Source.TypicalWeightPerUnit, 
+		LeadTimeDays = Source.LeadTimeDays,	
+		IsChillerStock = Source.IsChillerStock, 
+		TaxRate = Source.TaxRate, 
+		UnitPrice = Source.UnitPrice
+WHEN NOT MATCHED THEN
+
+INSERT
+([StockItemName], [SupplierID], [UnitPackageID], [OuterPackageID], [QuantityPerOuter], [TypicalWeightPerUnit], [LeadTimeDays],	[IsChillerStock], [TaxRate], [UnitPrice], [LastEditedBy])
+VALUES
+(Source.StockItemName, Source.SupplierID, Source.UnitPackageID, Source.OuterPackageID, Source.QuantityPerOuter, Source.TypicalWeightPerUnit, Source.LeadTimeDays,	Source.IsChillerStock, Source.TaxRate, Source.UnitPrice, 1)
+OUTPUT $action AS [OperationName]
+				  , Inserted.StockItemName
+                  , Deleted.StockItemName; 				  
+
+
 
 /*
 2. Выгрузить данные из таблицы StockItems в такой же xml-файл, как StockItems.xml
 */
 
-напишите здесь свое решение
+
+DECLARE @Query AS VARCHAR(2000), @Query2 AS VARCHAR(2000)
+SET @Query2 =  'SELECT StockItemName AS ""Item/@Name"", SupplierID AS ""Item/SupplierID"", UnitPackageID AS ""Item/Package/UnitPackageID"", OuterPackageID AS ""Item/Package/OuterPackageID"", QuantityPerOuter AS ""Item/Package/QuantityPerOuter"", TypicalWeightPerUnit AS ""Item/Package/TypicalWeightPerUnit"", LeadTimeDays AS ""Item/LeadTimeDays"", IsChillerStock AS ""Item/IsChillerStock"", TaxRate AS ""Item/TaxRate"", UnitPrice AS ""Item/UnitPrice"" FROM WideWorldImporters.Warehouse.StockItems FOR XML PATH('''') , ROOT(''StockItems''), TYPE;';
+SET @Query = 'bcp "' +  @Query2 + '" queryout "F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItemsExport.xml" -T -w';
+EXEC ..xp_cmdshell @Query
 
 
 /*
@@ -59,7 +200,13 @@ USE WideWorldImporters
 - FirstTag (из поля CustomFields, первое значение из массива Tags)
 */
 
-напишите здесь свое решение
+SELECT 
+	   StockItemID
+     , StockItemName
+	 , CustomFields
+	 , JSON_VALUE(CustomFields, '$.CountryOfManufacture') AS CountryOfManufacture
+	 , JSON_VALUE(CustomFields, '$.Tags[0]') AS FirstTag
+FROM Warehouse.StockItems
 
 /*
 4. Найти в StockItems строки, где есть тэг "Vintage".
@@ -80,5 +227,133 @@ USE WideWorldImporters
 ... where ... CustomFields like '%Vintage%' 
 */
 
+WITH cte
+AS
+(
+	SELECT 
+		   StockItemID
+		 , StockItemName
+		 , CustomFields
+		 , j2.Value AS Tag
+	FROM WideWorldImporters.Warehouse.StockItems
+	CROSS APPLY OPENJSON(CustomFields, '$.Tags') j2
+)
 
-напишите здесь свое решение
+SELECT 
+		StockItemID
+	  , StockItemName
+	  , STRING_AGG(Tag, ', ') AS Tags
+FROM cte
+WHERE StockItemID IN
+(
+	SELECT StockItemID
+	FROM cte
+	WHERE Tag = 'Vintage'
+)
+GROUP BY 
+		   StockItemID
+		 , StockItemName
+		 , CustomFields
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+/*
+SELECT STUFF
+(
+    (
+        SELECT CHAR(10) + tbl.TestText --you might use 13 and 10 here
+        FROM @tbl AS tbl
+        FOR XML PATH(''),TYPE
+    ).value('.','nvarchar(max)'),1,1,''
+)
+
+SELECT 
+	   StockItemName AS "Item/@Name"
+	   , SupplierID AS "Item/SupplierID"
+	   , UnitPackageID AS "Item/Package/UnitPackageID"
+	   , OuterPackageID AS "Item/Package/OuterPackageID"
+	   , QuantityPerOuter AS "Item/Package/QuantityPerOuter"
+	   , TypicalWeightPerUnit AS "Item/Package/TypicalWeightPerUnit"
+
+	   , LeadTimeDays AS "Item/LeadTimeDays"
+	   , IsChillerStock AS "Item/IsChillerStock"
+	   , TaxRate AS "Item/TaxRate"
+	   , UnitPrice AS "Item/UnitPrice"
+FROM Warehouse.StockItems FOR XML PATH('StockItems')
+
+
+DECLARE @Query AS VARCHAR(1000)
+SET @Query = 'bcp "SELECT * FROM [WideWorldImporters].[Warehouse].[StockItems] FOR XML PATH(''StockItems'')" queryout "F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItemsExport.xml" -T -c -t';
+
+EXEC ..xp_cmdshell @Query
+
+DECLARE @Query AS VARCHAR(2000)
+SET @Query = 'bcp "SELECT 
+StockItemName AS "Item/@Name"
+, SupplierID AS "Item/SupplierID"
+
+, UnitPackageID AS "Item/Package/UnitPackageID"
+, OuterPackageID AS "Item/Package/OuterPackageID"
+, QuantityPerOuter AS "Item/Package/QuantityPerOuter"
+, TypicalWeightPerUnit AS "Item/Package/TypicalWeightPerUnit"
+
+, LeadTimeDays AS "Item/LeadTimeDays"
+, IsChillerStock AS "Item/IsChillerStock"
+, TaxRate AS "Item/TaxRate"
+, UnitPrice AS "Item/UnitPrice"
+FROM Warehouse.StockItems FOR XML PATH(''StockItems'')" queryout "F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItemsExport.xml" -T -c -t';
+
+EXEC ..xp_cmdshell @Query
+*/
+
+/*
+SELECT 
+	   StockItemName AS "Item/@Name"
+	   , SupplierID AS "Item/SupplierID"
+	   , UnitPackageID AS "Item/Package/UnitPackageID"
+	   , OuterPackageID AS "Item/Package/OuterPackageID"
+	   , QuantityPerOuter AS "Item/Package/QuantityPerOuter"
+	   , TypicalWeightPerUnit AS "Item/Package/TypicalWeightPerUnit"
+	   , LeadTimeDays AS "Item/LeadTimeDays"
+	   , IsChillerStock AS "Item/IsChillerStock"
+	   , TaxRate AS "Item/TaxRate"
+	   , UnitPrice AS "Item/UnitPrice"
+FROM Warehouse.StockItems FOR XML PATH('StockItems')
+
+
+DECLARE @Query AS VARCHAR(2000), @Query2 AS VARCHAR(2000)
+SET @Query2 =  'SELECT 
+	   StockItemName AS "Item/@Name"
+	   , SupplierID AS "Item/SupplierID"
+	   , UnitPackageID AS "Item/Package/UnitPackageID"
+	   , OuterPackageID AS "Item/Package/OuterPackageID"
+	   , QuantityPerOuter AS "Item/Package/QuantityPerOuter"
+	   , TypicalWeightPerUnit AS "Item/Package/TypicalWeightPerUnit"
+
+	   , LeadTimeDays AS "Item/LeadTimeDays"
+	   , IsChillerStock AS "Item/IsChillerStock"
+	   , TaxRate AS "Item/TaxRate"
+	   , UnitPrice AS "Item/UnitPrice"
+FROM Warehouse.StockItems FOR XML PATH(''StockItems'')';
+
+
+SET @Query = 'bcp "' +  @Query2 + '" queryout "F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItemsExport.xml" -T -c -t';
+
+EXEC ..xp_cmdshell @Query
+
+
+DECLARE @Query AS VARCHAR(2000), @Query2 AS VARCHAR(2000)
+SET @Query2 =  'SELECT StockItemName AS "Item/@Name", SupplierID AS "Item/SupplierID", UnitPackageID AS "Item/Package/UnitPackageID", OuterPackageID AS "Item/Package/OuterPackageID", QuantityPerOuter AS "Item/Package/QuantityPerOuter", TypicalWeightPerUnit AS "Item/Package/TypicalWeightPerUnit", LeadTimeDays AS "Item/LeadTimeDays", IsChillerStock AS "Item/IsChillerStock", TaxRate AS "Item/TaxRate", UnitPrice AS "Item/UnitPrice" FROM Warehouse.StockItems FOR XML PATH(''StockItems'')';
+SET @Query = 'bcp "' +  @Query2 + '" queryout "F:\DB\OTUS\mssql-otus\homeworks\homework-9\StockItemsExport.xml" -T -c -t';
+EXEC ..xp_cmdshell @Query
+*/
+		 
